081c3478881b5208ec898392b05ddb7d79384064
Marco Silva <41647915+WokerSilva@users.noreply.github.com>
2022-04-27 01:39:29 -0500

    Agregando ejercicio 3

--- Practica04/practica04.hs
+++ Practica04/practica04.hs
@@ -40,6 +40,7 @@ instance Show Form where
   show (Ex x f) = "Ein " ++ x ++ " (" ++ show f ++ ")"
 
 
+
 --1. -alcance. Función que devuelve el alcance de los cuantificadores de
 --          una fórmula.
 -- alcance (All "x" (Ex "y" (Pr "P" [V "x", V "y"])))
@@ -47,8 +48,8 @@ alcance :: Form -> [(Form, Form)]
 alcance NForm = []
 alcance TrueF = []
 alcance FalseF = []
-alcance (Pr p f) = []
-alcance (Eq f1 f2) = []
+alcance (Pr p t) = []
+alcance (Eq t1 t2) = []
 alcance (Neg f) = alcance f
 alcance (Conj f1 f2) = []
 alcance (Disy f1 f2) = []
@@ -63,8 +64,8 @@ bv :: Form -> [Nombre]
 bv NForm = []
 bv TrueF = []
 bv FalseF = []
-bv (Pr p f) = []
-bv (Eq f1 f2) = []
+bv (Pr p t) = []
+bv (Eq t1 t2) = []
 bv (Neg f) = []
 bv (Conj f1 f2) = []
 bv (Disy f1 f2) = []
@@ -73,7 +74,7 @@ bv (Equi f1 f2) = []
 bv (Ex p f) = elimPropRep ([p] ++ bv f)
 bv (All p f) = elimPropRep ([p] ++ bv f)
 
--- Función auxiliar de < bv > para elimiar repetidos 
+-- Función auxiliar para elimiar repetidos 
 -- Esta función esta reciclada de la practica03
 elimPropRep :: Eq a => [a] -> [a]
 elimPropRep [] = []
@@ -85,7 +86,50 @@ elimPropRep (x:xs) = if elem x xs
 --3. -fv. Función que devuelve las variables libres de una fórmula.
 -- fv (Ex "y" (Pr "P" [V "x", V "z"]))
 fv :: Form -> [Nombre]
-fv f = error "Sin implementar."
+fv f = diferencia(varsFormu f) (bv f)
+
+-- 1ra Función Auxiliar
+-- Buscamos obtener todas las variables de la fórmula
+--  Usamos nuevamente elimPropRep 
+-- (Ex "x" (Pr "P" [V "x", V "y"]))
+varsFormu :: Form -> [Nombre]
+varsFormu NForm = []
+varsFormu TrueF = []
+varsFormu FalseF = []
+varsFormu (Pr p f) = varsConjunto f
+varsFormu (Eq f1 f2) = varsdTermino f1 ++ varsdTermino f2
+varsFormu (Neg f) = elimPropRep(varsFormu f)
+varsFormu (Conj f1 f2) = elimPropRep(varsFormu f1 ++ varsFormu f2)
+varsFormu (Disy f1 f2) = elimPropRep(varsFormu f1 ++ varsFormu f2)
+varsFormu (Imp f1 f2) = elimPropRep(varsFormu f1 ++ varsFormu f2)
+varsFormu (Equi f1 f2) = elimPropRep(varsFormu f1 ++ varsFormu f2)
+varsFormu (Ex x f) = elimPropRep(varsFormu f)
+varsFormu (All x f) = elimPropRep(varsFormu f)
+
+-- 2da Funcion Auxiliar 
+-- Separamos las variables de un termino.
+-- varsConjunto:  varsTerm (V "x")
+varsdTermino :: Term -> [Nombre]
+varsdTermino (V x) = [x]
+varsdTermino(F f []) = []
+varsdTermino (F f [x]) = varsdTermino x
+varsdTermino (F f (x:xs)) = varsdTermino x ++ varsConjunto xs
+
+
+-- 3ra Funcion Auxiliar
+-- Separamos las variables de un conjunto de terminos.
+-- varsConjunto [V "a", F "x" []]
+varsConjunto :: [Term] -> [Nombre]
+varsConjunto [x] = varsdTermino x
+varsConjunto (x:xs) = varsdTermino x ++ varsConjunto xs
+
+-- 4ta Funcion Auxiliar
+-- Regresamos la lista con la diferencia entre las variables 
+--  ligas y las libres
+diferencia :: Eq a => [a] -> [a] -> [a]
+diferencia xs ys = zs
+    where zs = [x | x <- xs, x `notElem` ys]
+
 
 --4. -sustTerm. Función que realiza la sustitución de variables en un término.
 -- sustTerm F "f" [V "x", F "a" []] [("x", V "y")]
