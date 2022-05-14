97ee07790412554d0c281aeec8410922f7f2efbf
Marco Silva <41647915+WokerSilva@users.noreply.github.com>
2022-05-14 16:25:15 -0500

    Agreando ejercicio 4

--- Practica05/practica05.pl
+++ Practica05/practica05.pl
@@ -1,6 +1,7 @@
 %Ejercicio 1
 %Devuelve true o false dependiendo si 
 %Un numero essta contenido en la lista
+%Prueba : elemento(3,[1,2,3,2]).
 
 elemento(X,[X|_]).
 elemento(X,[_|C]) :- elemento(X,C). 
@@ -12,6 +13,7 @@ elemento(X,[_|C]) :- elemento(X,C).
 %con la cual obtenemos el modulo del numero
 %en caso de ser primo se ira sumando hasta 
 %igualar el contador
+%Prueba : esprimo(7)
 
 esprimo(A) :- A > 1, compara(A,2).
 compara(A,B) :- 
@@ -21,5 +23,12 @@ compara(A,B) :-
 				-> false
 				; succ(B,C), compara(A,C).
 
+%Ejercicio 4
+%Eliminando duplicados de una lista
+%Donde: C: cabeza, Cl: cola, E: elemento
+%Y llamamos a la funcion del ejercicio 1
+%Prueba : eliminar([1,2,3,2], E).
 
-
+eliminar([],[]).
+eliminar([C|Cl],E) :- elemento(C,Cl), !, eliminar(Cl,E).
+eliminar([C|Cl],[C|E]) :- eliminar(Cl,E).
